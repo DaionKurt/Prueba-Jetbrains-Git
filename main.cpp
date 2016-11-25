0bbacaf24d315b47ae1050303034e10c0662477e
Carlos Fernández <carlosfdez@outlook.com>
2016-11-24 18:49:49 -0600

    Test func x14

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "addons.h"
 
+auto func(int a,int b)->int {return a+b;}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout<< "Hello "<<std::endl;
-    print("Hola mundo",1);
-    print("Sale bye");
-    print("HEHE");
+    std::cout<<"Hello, World!"<<std::endl;
+    std::cout<<"Hello "<<std::endl;
+    print(func(5,6));
     return 0;
 }
